fd196c458c3481d566e9579011771dcfc620e0b2
Lukáš Zvoníček <Lukas.Zvonicek.s01@osu.cz>
2026-03-19 23:36:49 +0100

    oprava webconfig

--- src/main/java/cz/osu/informatika/mapbackend/WebConfig.java
+++ src/main/java/cz/osu/informatika/mapbackend/WebConfig.java
@@ -10,11 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://127.0.0.1:5500",
-                        "http://localhost:5500",
-                        "https://tvoje-jmeno.github.io" // Sem dej adresu tvého budoucího webu
-                )
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
