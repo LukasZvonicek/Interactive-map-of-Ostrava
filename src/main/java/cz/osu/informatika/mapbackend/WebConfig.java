3755d6c3d1f631477222087efc604966af28ae3b
Lukáš Zvoníček <Lukas.Zvonicek.s01@osu.cz>
2026-03-19 18:54:07 +0100

    Příprava na hosting

--- src/main/java/cz/osu/informatika/mapbackend/WebConfig.java
+++ src/main/java/cz/osu/informatika/mapbackend/WebConfig.java
@@ -9,8 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Povolí všechny cesty (i vyhledávání, i analýzu)
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Povolí oba zápisy Live Serveru
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://127.0.0.1:5500",
+                        "http://localhost:5500",
+                        "https://tvoje-jmeno.github.io" // Sem dej adresu tvého budoucího webu
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
